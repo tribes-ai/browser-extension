8e42765a14257327de072a6db62d72fe824e2293
giteshk01 <gitesh@tribes.ai>
2022-01-19 21:17:20 +0530

    Removed content script from project

--- src/manifest.ts
+++ src/manifest.ts
@@ -4,7 +4,7 @@ import type PkgType from '../package.json'
 import { isDev, port, r } from '../scripts/utils'
 
 export async function getManifest() {
-  const pkg = await fs.readJSON(r('package.json')) as typeof PkgType
+  const pkg = (await fs.readJSON(r('package.json'))) as typeof PkgType
 
   // update this file to update this manifest.json
   // can also be conditional based on your need
@@ -31,31 +31,17 @@ export async function getManifest() {
       48: './assets/icon-512.png',
       128: './assets/icon-512.png',
     },
-    permissions: [
-      'tabs',
-      'storage',
-      'activeTab',
-      'http://*/',
-      'https://*/',
-    ],
-    content_scripts: [{
-      matches: ['http://*/*', 'https://*/*'],
-      js: ['./dist/contentScripts/index.global.js'],
-    }],
-    web_accessible_resources: [
-      'dist/contentScripts/style.css',
-    ],
+    permissions: ['tabs', 'storage', 'activeTab', 'http://*/', 'https://*/'],
   }
 
   if (isDev) {
     // for content script, as browsers will cache them for each reload,
     // we use a background script to always inject the latest version
     // see src/background/contentScriptHMR.ts
-    delete manifest.content_scripts
-    manifest.permissions?.push('webNavigation')
-
+    // delete manifest.content_scripts
+    // manifest.permissions?.push('webNavigation')
     // this is required on dev for Vite script to load
-    manifest.content_security_policy = `script-src \'self\' http://localhost:${port}; object-src \'self\'`
+    // manifest.content_security_policy = `script-src \'self\' http://localhost:${port}; object-src \'self\'`
   }
 
   return manifest
